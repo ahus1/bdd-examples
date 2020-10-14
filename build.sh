e988cfb35382eab5d0fec5851ec5427855612a45
Alexander Schwartz <alexander.schwartz@gmx.net>
2020-10-14 11:07:33 +0200

    synchronizing versions

--- build.sh
+++ build.sh
@@ -1,40 +1,43 @@
 #!/usr/bin/env bash
+set -e
+set -x
+
 cd cucumber-simple
-mvn -B clean test || exit $?
+mvn -B clean test
 cd ..
 
 cd gwen-calculator-junit
-mvn -B clean test || exit $?
+mvn -B clean test
 cd ..
 
 cd gwen-calculator-testng
-mvn -B clean test || exit $?
+mvn -B clean test
 cd ..
 
 cd gwen-calculator-scalatest
-mvn -B clean test || exit $?
+mvn -B clean test
 cd ..
 
 # cd gwen-arquillian
-# mvn -B clean test || exit $?
+# mvn -B clean test
 # cd ..
 
 cd jgiven-junit
-mvn -B clean test || exit $?
+mvn -B clean test
 cd ..
 
 cd jgiven-junit5
-mvn -B clean test || exit $?
+mvn -B clean test
 cd ..
 
 cd jgiven-arquillian
-mvn -B clean verify || exit $?
+mvn -B clean verify
 cd ..
 
 cd junit-bddstyle
-mvn -B clean verify || exit $?
+mvn -B clean verify
 cd ..
 
 cd junit5-bddstyle
-mvn -B clean verify || exit $?
+mvn -B clean verify
 cd ..
